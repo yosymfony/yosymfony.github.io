0b14b9a2d9eabaa5d555908ca8f552fcee498488
yosymfony <yosymfony@gmail.com>
2017-09-01 16:52:31 +0000

    Initial commit

---
+++ js/theme.js
@@ -0,0 +1,91 @@
+/*!
+ * Start Bootstrap - Creative Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+(function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    })
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Fit Text Plugin for Main Header
+    $("h1").fitText(
+        1.2, {
+            minFontSize: '35px',
+            maxFontSize: '65px'
+        }
+    );
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    });
+
+    $('#contactForm').submit(function(event) {
+    event.preventDefault(); // prevent default submit behaviour
+    // get values from FORM
+    var name = $("input#name").val();
+    var email = $("input#email").val();
+    var message = $("textarea#message").val();
+    var firstName = name; // For Success/Failure Message
+    // Check for white space in name for Success/Fail message
+    if (firstName.indexOf(' ') >= 0) {
+        firstName = name.split(' ').slice(0, -1).join(' ');
+    }
+    $.ajax({
+        url: "http://getsimpleform.com/messages/?form_api_token=3065aa656aabf5ed1c2a9230594ce9f7",
+        type: "POST",
+        data: {
+            name: name,
+            email: email,
+            message: message
+        },
+        cache: false,
+        success: function() {
+            // Success message
+            $('#success').html("<div class='alert alert-success'>");
+            $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                .append("</button>");
+            $('#success > .alert-success')
+                .append("<strong>Your message has been sent. Thanks! We will answer you as soon as possible.</strong>");
+            $('#success > .alert-success')
+                .append('</div>');
+
+            //clear all fields
+            $('#contactForm').trigger("reset");
+        },
+        error: function() {
+            // Fail message
+            $('#success').html("<div class='alert alert-danger'>");
+            $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                .append("</button>");
+            $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+            $('#success > .alert-danger').append('</div>');
+            //clear all fields
+            $('#contactForm').trigger("reset");
+        },
+    });
+  })
+
+})(jQuery); // End of use strict
